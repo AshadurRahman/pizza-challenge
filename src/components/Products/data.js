a56d1646bfd1cf42fa50ddc1f724c9a394b70339
AshadurRahman <rshozib@gmail.com>
2021-07-31 18:18:19 +0200

    product data added

---
+++ src/components/Products/data.js
@@ -0,0 +1,27 @@
+import product1 from '../../images/product-1.jpg'
+import product2 from '../../images/product-2.jpg'
+import product3 from '../../images/product-3.jpg'
+
+export const productData = [
+    {
+        img: product1,
+        alt: 'Pizza',
+        name: 'Small Pizza',
+        price: '$14.99',
+        button: 'Place Order'
+    },
+    {
+        img: product2,
+        alt: 'Pizza',
+        name: 'Medium Pizza',
+        price: '$16.99',
+        button: 'Place Order'
+    },
+    {
+        img: product3,
+        alt: 'Pizza',
+        name: 'Large Pizza',
+        price: '$19.99',
+        button: 'Place Order'
+    },
+]
