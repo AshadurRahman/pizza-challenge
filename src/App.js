dfd0312a235f493f7dcfb1d12498c3a00330a52e
AshadurRahman <rshozib@gmail.com>
2021-08-01 22:18:40 +0200

    unused imports removed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { GlobalStyle } from './globalStyle';
-import Hero from './components/Hero';
-import Products from './components/Products';
-import { getProducts } from './components/Products/data';
 import Address from './components/Address';
 import Home from './components/Home';
 
--- src/components/Address/index.js
+++ src/components/Address/index.js
@@ -1,9 +1,68 @@
 import React from 'react';
 
 const Address = () => {
+    let addresses = [];
+
+    const addressInfo = (ev) => {
+        ev.preventDefault();
+        let address = {
+            id: Date.now(),
+            name: document.getElementById('name').value,
+            street: document.getElementById('street').value,
+            house: document.getElementById('house').value,
+            post: document.getElementById('post').value,
+            city: document.getElementById('city').value,
+            mobile: document.getElementById('mobile').value,
+        }
+        addresses.push(address);
+        // document.forms[0].reset();
+
+        console.warn('Added', { addresses });
+        let pre = document.querySelector(`msg pre`);
+        pre.textContent = '\n' + JSON.stringify(addresses, '\t', 2);
+    }
+    const addAddress = () => {
+
+        document.addEventListener('DOMContentLoaded', () => {
+            document.getElementById('btn').addEventListener('click', addressInfo);
+        })
+    }
+
     return (
         <div>
-            <h1> Address </h1>
+            <h1> Provide the Delivery Address </h1>
+            <form>
+                <div>
+                    <label>Full Name: </label>
+                    <input type="text" id="name" placeholder="Name" />
+                </div>
+                <div>
+                    <label>Street Name: </label>
+                    <input type="text" id="street" placeholder="Street" />
+                </div>
+                <div>
+                    <label>House Number: </label>
+                    <input type="text" id="house" placeholder="House Number" />
+                </div><div>
+                    <label>Postal Code: </label>
+                    <input type="text" id="post" placeholder="Postal Code" />
+                </div>
+                <div>
+                    <label>City: </label>
+                    <input type="text" id="city" placeholder="City" />
+                </div>
+                <div>
+                    <label>Mobile: </label>
+                    <input type="text" id="mobile" placeholder="Mobile" />
+                </div>
+                <div>
+                    <button onClick={addAddress}>Submit</button>
+                </div>
+                <div id="msg">
+                    <pre></pre>
+                </div>
+
+            </form>
         </div>
     );
 }
