279234365750c58c20de70428df33d25c9f89795
AshadurRahman <rshozib@gmail.com>
2021-08-03 01:09:04 +0200

    some test added

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { getProducts } from './components/Products/data';
+import Address from './components/Address/index';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('pizza lists', () => {
+  expect(getProducts).toMatchSnapshot();
+});
+
+var pizzas = getProducts();
+
+for (let i = 0; i < pizzas.length; i += 1) {
+  it(`products[${i}] should have properties(id, name, img, desc, price)`, () => {
+    expect(pizzas[i]).toHaveProperty('id');
+    expect(pizzas[i]).toHaveProperty('name');
+    expect(pizzas[i]).toHaveProperty('img');
+    expect(pizzas[i]).toHaveProperty('desc');
+    expect(pizzas[i]).toHaveProperty('price');
+  });
+}
+
+test('map calls its argument with a non-null argument', () => {
+  const mock = jest.fn();
+  [1].map(x => mock(x));
+  expect(mock).toBeCalledWith(expect.anything());
 });
